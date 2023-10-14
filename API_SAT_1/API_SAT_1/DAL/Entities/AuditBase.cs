5a35de0d4bb6873743d55389b967b3e48e4d8051
Yonnier Ivan Vega Palacios <yonnivanvp@hotmail.com>
2023-10-14 14:21:25 -0500

    Create Country Class

--- API_SAT_1/API_SAT_1/DAL/Entities/AuditBase.cs
+++ API_SAT_1/API_SAT_1/DAL/Entities/AuditBase.cs
@@ -1,6 +1,13 @@
-﻿namespace API_SAT_1.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_SAT_1.DAL.Entities
 {
     public class AuditBase
     {
+        [Key] //DataAnnotation me sirve para definir que esta propiedad ID es un PK
+        [Required] //Para campos obligatorios, osea que deben tener un valor (No permite nulls)
+        public virtual Guid Id { get; set; } //Será la PK de tosas las tablas de mi BD
+        public virtual DateTime? CreatedDate { get; set; } //Campos nuleables, notacion elvis (?) permite guardar nulos
+        public virtual DateTime? ModifiedDate { get; set; } //Campos nuleables
     }
 }
---
+++ API_SAT_1/API_SAT_1/DAL/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Timers;
+
+namespace API_SAT_1.DAL.Entities
+{
+    public class Country : AuditBase
+    {
+        [Display(Name = "Pais")] // Para pintar el nombre en el FrontEnd
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")] //Longitud de caracteres maxima que esta propiedad me permite tener, Ej: varchar(50)
+        [Required(ErrorMessage = "¡El campo {0} es obligatorio!")]
+        public String Name { get; set; } //varchar(50)
+    }
+}

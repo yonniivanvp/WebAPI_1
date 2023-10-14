e605b2ed22e9ee1dcdd678802c6778a4d0ee4efd
Yonnier Ivan Vega Palacios <yonnivanvp@hotmail.com>
2023-10-14 16:06:07 -0500

    Create database dependency

---
+++ API_SAT_1/API_SAT_1/DAL/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using API_SAT_1.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_SAT_1.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        //Con este constructor me podré conectar a la BD, me brinda unas opciones de configuracion que ya estan definidas internamente
+        public DataBaseContext(DbContextOptions<DataBaseContext>options) : base(options) 
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; } //Esta linea me toma la clase Country y me la mapea en SQL SERVER para crear una tabla llamada COUNTRIES
+
+    }
+}
